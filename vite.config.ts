09f92f69c68bc53f80de399c7ae09c8c3c936547
arbey dzib <arbeydzib1@gmail.com>
2025-09-23 19:01:59 -0500

    frontend final config

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,17 @@ export default defineConfig({
     react(), 
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0', // Permite conexiones externas
+    port: 5173, // Puerto por defecto de Vite
+    strictPort: true, // Falla si el puerto no está disponible
+    hmr: {
+      port: 5173
+    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: true
+  }
 })
