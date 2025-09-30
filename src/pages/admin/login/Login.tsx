ade58213c6b9a194ccbcef1436e57dac11aa190f
NierRep24 <danielsanchezmartinez064@gmail.com>
2025-09-29 18:53:19 -0600

    textura en login

--- src/pages/admin/login/Login.tsx
+++ src/pages/admin/login/Login.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import texture from "../../../assets/image/image.png";
+import loginImage from '../../../assets/image/agroimage.png';
 import AdminLoginForm from "./components/adminLoginForm";
-import AgroImage from "../../../assets/image/agroimage.png";
 const Login: React.FC = () => {
   return (
-      <div className="grid grid-cols-2 w-screen h-screen overflow-hidden bg-[#F2FFEF]">
+      <div className="grid grid-cols-2 w-screen h-screen overflow-hidden">
+          <img
+            src={texture}
+            alt="textura"
+            className="absolute opacity-10 w-full h-full object-cover"
+          />
         <section className=" relative">
         <AdminLoginForm/>
         </section>
 
         <section className="z-10 w-full">
-          <img src={AgroImage} className="w-full" alt="AgroImage" />
+          <img src={loginImage} className="w-full" alt="loginimage" />
         </section>
       </div>
   );
